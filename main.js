5a7244154580b799de382b8f10d753ce5ee8869a
rober99.guedes@gmail.com <rober99.guedes@gmail.com>
2024-03-04 21:41:18 -0600

    Hasta aqui segui el tutorial,minuto 2_55_58,Voy a hacerlo por mi cuenta

---
+++ main.js
@@ -0,0 +1,8 @@
+const jsonForm = document.querySelector("#jsonform");
+const csvForm = document.querySelector("#csvform");
+const bConvert = document.querySelector("#bConvert");
+
+bConvert.addEventListener("click", (e) => {
+  console.log("lkasfjdkj");
+  convertJSONtoCSV();
+});
